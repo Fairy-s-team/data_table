cb027e64c974d9f418bf751798fc5580de303613
ldq1999 <38351236+ldq1999@users.noreply.github.com>
2020-04-14 10:13:41 +0800

    Add files via upload

---
+++ evaluation_system_teacher_info.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.19, for Win64 (x86_64)
+--
+-- Host: localhost    Database: evaluation_system
+-- ------------------------------------------------------
+-- Server version	8.0.19
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `teacher_info`
+--
+
+DROP TABLE IF EXISTS `teacher_info`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teacher_info` (
+  `teacherId` char(10) COLLATE utf8_bin NOT NULL COMMENT 'teacher number',
+  `teacherName` varchar(10) COLLATE utf8_bin NOT NULL COMMENT 'teacher name',
+  `password` varchar(10) COLLATE utf8_bin NOT NULL COMMENT 'teahcer login password',
+  `identity` char(23) COLLATE utf8_bin NOT NULL COMMENT 'teacher identity',
+  PRIMARY KEY (`teacherId`),
+  UNIQUE KEY `teacherId_UNIQUE` (`teacherId`),
+  UNIQUE KEY `identity_UNIQUE` (`identity`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='老师基本信息和登录信息';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teacher_info`
+--
+
+LOCK TABLES `teacher_info` WRITE;
+/*!40000 ALTER TABLE `teacher_info` DISABLE KEYS */;
+/*!40000 ALTER TABLE `teacher_info` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-04-13 23:24:23
