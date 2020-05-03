48b2be2ec3d194326fc8c1bb13e7f92b0b704ee4
Leonor <you@example.com>
2020-05-03 17:57:23 +0800

    first

---
+++ admin_role_user.sql
@@ -0,0 +1,6 @@
+INSERT INTO `admin_role_user` VALUES (1, '20000721', 1);
+INSERT INTO `admin_role_user` VALUES (2, '20000731', 1);
+INSERT INTO `admin_role_user` VALUES (3, '20041231', 3);
+INSERT INTO `admin_role_user` VALUES (4, '20041231', 3);
+INSERT INTO `admin_role_user` VALUES (5, '2018110338', 2);
+INSERT INTO `admin_role_user` VALUES (6, '2018110339', 2);
